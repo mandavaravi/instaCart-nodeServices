f92f0dd452e2b21030bbe7901ee37e06845c132e
Narenkassyap <narenkassyap05@gmail.com>
2023-04-30 16:43:22 -0400

    More functionalities

--- api.js
+++ api.js
@@ -180,6 +180,70 @@ app.post('/add_update_inv', (req, res) => {
     });
 });
 
+//user retailer order interaction
+app.post('/order', (req, res) => {
+    fs.readFile('./order.json', (err, fileData) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading file');
+        }
+
+        let existingData = [];
+        try {
+            existingData = JSON.parse(fileData);
+        } catch (err) {
+            console.error(err);
+            return res.status(500).send('Error parsing JSON file');
+        }
+
+        const response = req.body;
+
+        if (response.type === "add") {
+            existingData[response.retailerId] = [...existingData[response.retailerId], ...response.itemList];
+            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data appended to file');
+                res.send('Data appended to file');
+            });
+        } else if (response.type === "update") {
+            for (let i = 0; i < existingData[response.retailerId].length; i++) {
+                if (existingData[response.retailerId][i].itemId === response.itemId) {
+                    existingData[response.retailerId][i].quantity = response.quantity;
+                    existingData[response.retailerId][i].pricePerQuantity = response.pricePerQuantity;
+                }
+            }
+            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data updated in file');
+                res.send('Data updated in file');
+            });
+        } else if (response.type === "delete") {
+            filteredItems = existingData[response.retailerId].filter(item => item.itemId !== response.itemId);
+            existingData[response.retailerId] = filteredItems;
+            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data deleted in file');
+                res.send('Data deleted in file');
+            });
+        } else if (response.type === "viewinv") {
+            res.send(existingData[response.retailerId])
+            
+        } else {
+            res.send('Invalid request type');
+        }
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
