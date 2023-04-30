29b98e25a0f6c6124d7721b584252b0a613d1284
Narenkassyap <narenkassyap05@gmail.com>
2023-04-30 17:07:47 -0400

    no changes

--- api.js
+++ api.js
@@ -16,7 +16,6 @@ app.post('/api/data', (req, res) => {
     res.send(jsonData);
 });
 
-
 // POST endpoint to receive JSON data and append it to a local JSON file
 app.post('/api/data1', (req, res) => {
     const jsonData = req.body;
@@ -108,7 +107,7 @@ app.post('/add_update_cart', (req, res) => {
                 console.log('Data deleted in file');
                 res.send('Data deleted in file');
             });
-        } 
+        }
         else if (response.type === "viewcart") {
             res.send(existingData[response.userId])
         } else {
@@ -173,7 +172,7 @@ app.post('/add_update_inv', (req, res) => {
             });
         } else if (response.type === "viewinv") {
             res.send(existingData[response.retailerId])
-            
+
         } else {
             res.send('Invalid request type');
         }
@@ -196,11 +195,12 @@ app.post('/order', (req, res) => {
             return res.status(500).send('Error parsing JSON file');
         }
 
-        const response = req.body;
 
+        const response = req.body;
+        //after order completes
         if (response.type === "add") {
-            existingData[response.retailerId] = [...existingData[response.retailerId], ...response.itemList];
-            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
+            existingData[response.userId][response.orderId] = response.itemList;
+            fs.writeFile('./order.json', JSON.stringify(existingData, null, 2), (err) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).send('Error writing file');
@@ -208,44 +208,157 @@ app.post('/order', (req, res) => {
                 console.log('Data appended to file');
                 res.send('Data appended to file');
             });
-        } else if (response.type === "update") {
-            for (let i = 0; i < existingData[response.retailerId].length; i++) {
-                if (existingData[response.retailerId][i].itemId === response.itemId) {
-                    existingData[response.retailerId][i].quantity = response.quantity;
-                    existingData[response.retailerId][i].pricePerQuantity = response.pricePerQuantity;
-                }
-            }
-            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).send('Error writing file');
-                }
-                console.log('Data updated in file');
-                res.send('Data updated in file');
-            });
-        } else if (response.type === "delete") {
-            filteredItems = existingData[response.retailerId].filter(item => item.itemId !== response.itemId);
-            existingData[response.retailerId] = filteredItems;
-            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).send('Error writing file');
-                }
-                console.log('Data deleted in file');
-                res.send('Data deleted in file');
-            });
-        } else if (response.type === "viewinv") {
-            res.send(existingData[response.retailerId])
-            
+        } else if (response.type === "vieworder") {
+            res.send(existingData[response.userId])
+
         } else {
             res.send('Invalid request type');
         }
     });
 });
 
+//api to add new user
+app.post('/newUser_Retailer', (req, res) => {
+
+    const response = req.body;
+    if (response.type === "user") {
+        fs.readFile('./userToCart.json', (err, fileData) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error reading file');
+            }
+
+            let existingData = [];
+            try {
+                existingData = JSON.parse(fileData);
+            } catch (err) {
+                console.error(err);
+                return res.status(500).send('Error parsing JSON file');
+            }
+            //cretae new cart for user
+            existingData[response.userId] = [];
+
+            fs.writeFile('./userToCart.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data appended to file');
+                //res.send('Data appended to file');
+            });
+        });
+        //store user details
+        // //add retailer details
+        fs.readFile('./userDetails.json', (err, fileData) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error reading file');
+            }
+
+            let existingData = [];
+            try {
+                existingData = JSON.parse(fileData);
+            } catch (err) {
+                console.error(err);
+                return res.status(500).send('Error parsing JSON file');
+            }
+            //cretae new details for retailer
+            // existingData[response.retailerId].retailerId = response.retailerId
+               
+                
+            // existingData[response.retailerId].retailerName = response.retailerName
+            // existingData[response.retailerId].retailerImage = response.retailerImage
+            // existingData[response.retailerId].homeAddress = response.homeAddress
+
+            existingData[response.userId] = {
+                "userId": response.userId,
+                "userName": response.userName,
+                "homeAddress": response.homeAddress
+            }
+            
+            
+
+            fs.writeFile('./userDetails.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data appended to file');
+                res.send('Data appended to file');
+            });
+        });
+    }
+    else if (response.type === "retailer"){
+        fs.readFile('./retailer.json', (err, fileData) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error reading file');
+            }
+
+            let existingData = [];
+            try {
+                existingData = JSON.parse(fileData);
+            } catch (err) {
+                console.error(err);
+                return res.status(500).send('Error parsing JSON file');
+            }
+            //cretae new cart for user
+            existingData[response.retailerId] = [];
+
+            fs.writeFile('./retailer.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data appended to file');
+                //res.send('Data appended to file');
+            });
+        });
+
+        // //add retailer details
+        fs.readFile('./retailerDetails.json', (err, fileData) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error reading file');
+            }
+
+            let existingData = [];
+            try {
+                existingData = JSON.parse(fileData);
+            } catch (err) {
+                console.error(err);
+                return res.status(500).send('Error parsing JSON file');
+            }
+            //cretae new details for retailer
+            // existingData[response.retailerId].retailerId = response.retailerId
+               
+                
+            // existingData[response.retailerId].retailerName = response.retailerName
+            // existingData[response.retailerId].retailerImage = response.retailerImage
+            // existingData[response.retailerId].homeAddress = response.homeAddress
+
+            existingData[response.retailerId] = {
+                "retailerId": response.retailerId,
+                "retailerName": response.retailerName,
+                "retailerImage": "https://i.pinimg.com/originals/66/f7/72/66f77296282b5ab7c2780724802614c0.png",
+                "homeAddress": response.homeAddress
+            }
+            
+            
+
+            fs.writeFile('./retailerDetails.json', JSON.stringify(existingData, null, 2), (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error writing file');
+                }
+                console.log('Data appended to file');
+                res.send('Data appended to file');
+            });
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-//api to 
